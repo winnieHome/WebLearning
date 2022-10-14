addf3fc6a5160067235bafd26c1fbc8d619b9191
napianlvye <xiaoxiaoli5987@163.com>
2022-10-14 14:36:36 +0800

    saved

---
+++ js-site/引入方式/cite.js
@@ -0,0 +1,4 @@
+var div = document.getElementById('cite-use');
+var node = document.createTextNode("This is cite js document");
+div.appendChild(node);
+console.log("😆 --- 执行4");
---
+++ js-site/引入方式/gibberish.js
@@ -0,0 +1,4 @@
+
+var div = document.getElementById('dynamic-cite');
+var node = document.createTextNode("This is gibberish.js,This is dynamic-cite");
+div.appendChild(node);
